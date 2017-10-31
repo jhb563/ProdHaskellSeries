f69c8f94f3054aa4d0bcd3c539c864e594fbd4fd
jhb563 <jhbowen047@gmail.com>
2017-10-30 20:37:45 -0700

    Implement testing w/o postgres and redis.

--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -1,17 +1,24 @@
+{-# LANGUAGE TypeOperators #-}
+
 module TestUtils where
 
-import Control.Concurrent (forkIO, ThreadId, threadDelay)
+import Control.Concurrent (forkIO, ThreadId, threadDelay, newMVar, MVar)
 import Control.Monad.Logger (runStdoutLoggingT)
 import Control.Monad.Reader (runReaderT)
+import qualified Data.Map as Map
 import Database.Persist.Postgresql (withPostgresqlConn, runMigrationSilent)
 import Network.HTTP.Client (newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.Wai.Handler.Warp (run)
+import Servant.API
 import Servant.Client (ClientEnv(..), parseBaseUrl)
+import Servant.Server (serve, Server, (:~>), Handler, enter)
 
-import API (runServer)
+import API
 import Cache (RedisInfo, fetchRedisConnection)
 import Database (PGInfo, fetchPostgresConnection)
 import Schema (migrateAll)
+import TestMonad (transformTestToHandler, TestMonad, UserMap, ArticleMap)
 
 setupTests :: IO (PGInfo, RedisInfo, ClientEnv, ThreadId)
 setupTests = do
@@ -25,3 +32,25 @@ setupTests = do
   tid <- forkIO runServer
   threadDelay 1000000
   return (pgInfo, redisInfo, clientEnv, tid)
+
+testAPIServer :: (TestMonad :~> Handler) -> Server FullAPI
+testAPIServer nt =
+  enter nt $
+    fetchUsersHandler :<|>
+    createUserHandler :<|>
+    fetchArticleHandler :<|>
+    createArticleHandler :<|>
+    fetchArticlesByAuthorHandler :<|>
+    fetchRecentArticlesHandler
+
+setupTests' :: IO (ClientEnv, MVar (UserMap, ArticleMap, UserMap), ThreadId)
+setupTests' = do
+  mgr <- newManager tlsManagerSettings
+  baseUrl <- parseBaseUrl "http://127.0.0.1:8000"
+  let clientEnv = ClientEnv mgr baseUrl
+  let initialMap = (Map.empty, Map.empty, Map.empty)
+  mapRef <- newMVar initialMap
+  tid <- forkIO $
+    run 8000 (serve usersAPI (testAPIServer (transformTestToHandler mapRef)))
+  threadDelay 1000000
+  return (clientEnv, mapRef, tid)
